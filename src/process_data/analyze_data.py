78acbe9d59b94254709465b84abefda9a632514e
pepe <228986825335-compute@developer.gserviceaccount.com>
2023-06-25 03:40:45 +0200

    First commit for word count processing for tweets csv

--- src/process_data/analyze_data.py
+++ src/process_data/analyze_data.py
@@ -4,26 +4,29 @@ from src.process_data.generate_wc import generate_wc, generate_wf_hist, create_f
 from src.preprocessing.preprocess_utils import create_tweet_list
 from src.utils.support_functions import check_empty_text, get_clusters, print_list_element
 
-tweets_df = prepare_data('C:\\Users\\juanma\\Desktop\\fp2\\activities\\activity_4\\data\\twitter_reduced.zip')
 
-print("Starting data process")
-print("There are {} clusters in this datasets".format(len(get_clusters(tweets_df))))
+# tweets_df = prepare_data('C:\\Users\\juanma\\Desktop\\fp2\\activities\\activity_4\\data\\twitter_reduced.zip')
+def analyze_data(csv_path='C:\\Users\\juanma\\Desktop\\fp2\\activities\\activity_4\\data\\twitter_reduced.zip'):
 
-# Muestra mensajes del ejercicio
-check_empty_text(tweets_df)
-print_list_element(create_tweet_list(tweets_df))
-for c in get_clusters(tweets_df):
-    print("Generating word frequency dictionary")
-    tf_list = create_tweet_list(tweets_df.loc[tweets_df["sentiment"] == c])
-    tf_dict = create_freq_reduce(tf_list)
-    print("Word frequency dictionary generated")
+    tweets_df = prepare_data(csv_path)
+    print("Starting data process")
+    print("There are {} clusters in this datasets".format(len(get_clusters(tweets_df))))
 
-    print("Generating word cloud fro cluster {}".format(c))
-    wordcloud = generate_wc(tf_dict)
-    wc_to_file(wordcloud, c)
-    print("Word cloud for cluster {} generated in data output path".format(c))
+    # Muestra mensajes del ejercicio
+    check_empty_text(tweets_df)
+    print_list_element(create_tweet_list(tweets_df))
+    for c in get_clusters(tweets_df):
+        print("Generating word frequency dictionary")
+        tf_list = create_tweet_list(tweets_df.loc[tweets_df["sentiment"] == c])
+        tf_dict = create_freq_reduce(tf_list)
+        print("Word frequency dictionary generated")
 
-    print("Generating word frequency plot for cluster {}".format(c))
-    wf_plot = generate_wf_hist(tf_dict)
-    hist_to_file(wf_plot, c)
-    print("Word frequency plot for cluster {} generated in data output path".format(c))
+        print("Generating word cloud fro cluster {}".format(c))
+        wordcloud = generate_wc(tf_dict)
+        wc_to_file(wordcloud, c)
+        print("Word cloud for cluster {} generated in data output path".format(c))
+
+        print("Generating word frequency plot for cluster {}".format(c))
+        wf_plot = generate_wf_hist(tf_dict)
+        hist_to_file(wf_plot, c)
+        print("Word frequency plot for cluster {} generated in data output path".format(c))
